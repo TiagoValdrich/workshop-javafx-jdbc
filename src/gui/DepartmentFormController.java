6f90904fd962cb8f371bf166c9820d8a596410b6
Tiago Valdrich <tiago.valdrich@hotmail.com>
2019-02-03 20:01:45 -0200

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,12 +1,18 @@
 package gui;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,6 +20,7 @@ import java.util.ResourceBundle;
 public class DepartmentFormController implements Initializable {
 
     private Department entity;
+    private DepartmentService departmentService;
 
     @FXML
     private TextField txtId;
@@ -34,14 +41,44 @@ public class DepartmentFormController implements Initializable {
         this.entity = entity;
     }
 
-    @FXML
-    public void onBtSaveAction() {
-        System.out.println("Hello");
+    public void setDepartmentService(DepartmentService departmentService) {
+        this.departmentService = departmentService;
     }
 
     @FXML
-    public void onBtCancelAction() {
-        System.out.println("Hello2");
+    public void onBtSaveAction(ActionEvent event) {
+        if (entity == null) {
+            throw new IllegalStateException("Entity is null!");
+        }
+
+        if (departmentService == null) {
+            throw new IllegalStateException("Service is null!");
+        }
+
+        try {
+
+            entity = getFormData();
+            departmentService.saveOrUpdate(entity);
+            Utils.currentStage(event).close();
+
+        } catch (DbException e) {
+            Alerts.showAlert("Error saving department", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
+
+    }
+
+    private Department getFormData() {
+        Department department = new Department();
+
+        department.setId(Utils.tryParseToInt(txtId.getText()));
+        department.setName(txtName.getText());
+
+        return department;
+    }
+
+    @FXML
+    public void onBtCancelAction(ActionEvent event) {
+        Utils.currentStage(event).close();
     }
 
     @Override
